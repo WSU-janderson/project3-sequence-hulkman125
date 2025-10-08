9f0b2681a1017f49274999917856b75bdfc3c03f
Tyler Harvey <111830252+hulkman125@users.noreply.github.com>
2025-10-08 16:44:52 -0400

    here are the changes

--- SequenceDebug.cpp
+++ SequenceDebug.cpp
@@ -8,10 +8,61 @@
  */
 #include <iostream>
 
+#include "Sequence.h"
+
 using namespace std;
 
 int main() {
-    cout << "Your project is ready to go!" << endl;
+
+    Sequence X(0);
+    X.push_back("milk");
+    X.push_back("eggs");
+    X.push_back("bananas");
+    //X.pop_back();
+
+    Sequence Y(0);
+    Y.push_back("bread");
+    Y.push_back("cookies");
+    Y.push_back("cake");
+    Y.push_back("cheese");
+    Y.push_back( "chocolate");
+
+    X = Y;
+    cout << X << endl;
+    cout << Y << endl;
+
+
+    /*cout << Y[4] << endl;
+
+    //Y.erase(4,2);
+
+
+    //Y.clear();
+
+    cout << "Front is:   " << Y.front() << endl;
+    cout << "Back is:    " << Y.back() << endl;
+
+    if (Y.empty()) {
+        cout<< "Y is empty" << endl;
+    }
+    else {
+        cout << "Y is not empty" << endl;
+    }
+
+    cout << "Size is:     " << Y.size() << endl;
+    Y.erase(2,2);
+    cout << Y << endl;
+    Y = X;
+
+    cout << Y << endl;
+    cout << "Size is:     " << Y.size() << endl;
+    Y.clear();
+    if (Y.empty()) {
+        cout<< "Y is empty" << endl;
+    }
+    else {
+        cout << "Y is not empty" << endl;
+    }*/
 
     return 0;
 }
